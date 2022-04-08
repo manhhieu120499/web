597df7193f9e67354e52332d4c90216cbcbf8164
voman <vomanhhieu62@gmail.com>
2022-04-08 13:40:57 +0600

    add all file

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Trúc';
+const giftUrl = 'https://www.facebook.com/chucxuxinhdepcute174';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Trúc';
+const contentCard = 'Chúc Trúc có một ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với mày trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "girl.jpg";
+// const base64 = '';
+// const giftImageBase64 = "data:image/png;base64, " + base64;
